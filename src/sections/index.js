9354ce2cfad554bef82a80682f16422b9cf84c09
Hari Nair <nairgh@hotmail.com>
2024-08-25 10:34:28 -0400

    Initial Commit

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Footer,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+};
